78ffb2cce10df2ccb7675261c3dfabcb1da314a0
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:39:08 +0200

    Add piece, title, description, spin circle, and menu to main view

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,22 @@
 <x-app-layout>
 
-    Main
+    <x-slot name="piece">
+        <div class="piece">
+            <img src="images/part.png" alt="">
+        </div>
+    </x-slot>
+
+    <h1 class="main-title">
+        Инженерно-строительные изыскания
+    </h1>
+
+    <p class="description">
+        Подразделения организации укомплектованы высококвалифицированными специалистами, оснащены новейшим оборудованием и используют
+        современные методы производства и обработки материалов.
+    </p>
+
+    <x-spin-circle />
+
+    <x-menu />
 
 </x-app-layout>
