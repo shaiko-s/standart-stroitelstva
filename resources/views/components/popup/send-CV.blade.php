ba84c6743adcd2c0bdaf1f596c4a37db9938910f
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:41:55 +0200

    Add send CV popup component

---
+++ resources/views/components/popup/send-CV.blade.php
@@ -0,0 +1,28 @@
+<!-- Menu PopUp -->
+<div id="sendCV" class="popup">
+
+    <x-popup.button-close id="closeSendCV" />
+
+    <div class="popup-container">
+
+        <h3>Отправьте нам свое резюме</h3>
+
+        <p>Оставьте Ваши данные и мы свяжемся с Вами</p>
+
+        <div class="input-field">
+            <input type="text" id="name" name="name" placeholder="Введите Ваше имя" required>
+        </div>
+
+        <div class="input-field">
+            <input type="tel" id="phone" name="phone" placeholder="Введите Ваш телефон" required>
+        </div>
+
+        <x-button-attachCV id="attachCV" />
+
+        <x-button-sendCV id="confirmSendCV" title="Отправить" />
+
+        <p class="declaration">Отправляя заявку, Вы соглашаетесь
+            с <a href="#">политикой конфиденциальности</a></p>
+
+    </div>
+</div>
