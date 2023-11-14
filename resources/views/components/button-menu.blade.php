c3e94a44a15841f1e581e249077c038c6cbb627d
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:40:01 +0200

    Add button menu component

---
+++ resources/views/components/button-menu.blade.php
@@ -0,0 +1,8 @@
+<button id="openMenu" class="menuButton">
+    <svg xmlns="http://www.w3.org/2000/svg" width="38" height="39" viewBox="0 0 38 39" fill="none">
+        <circle cx="19" cy="19.457" r="18.5" stroke="white" stroke-opacity="0.3"/>
+        <rect x="13.9736" y="13.3726" width="12.6726" height="1.35209" fill="white"/>
+        <rect x="10.0017" y="18.781" width="16.6445" height="1.35209" fill="white"/>
+        <rect x="16.8108" y="24.1893" width="9.83534" height="1.35209" fill="white"/>
+    </svg>
+</button>
