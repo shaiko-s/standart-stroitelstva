e28b3ff36dd56bc132bb91d383631f59e218e3b0
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:41:22 +0200

    Add spin-circle component to views

---
+++ resources/views/components/spin-circle.blade.php
@@ -0,0 +1,22 @@
+<div class="circle">
+    <div class="black-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" width="154" height="154" viewBox="0 0 154 154" fill="none">
+            <path d="M152.91 76.8572C152.91 118.713 118.98 152.644 77.124 152.644C35.2683 152.644 1.33765 118.713 1.33765 76.8572C1.33765 35.0016 35.2683 1.07092 77.124 1.07092C118.98 1.07092 152.91 35.0016 152.91 76.8572Z" stroke="#CAD5DF"/>
+        </svg>
+    </div>
+    <div class="text">
+        <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140">
+        <defs>
+            <path id="myCircle" d="M70,70 m-57.5,0 a57.5,57.5 0 1,1 115,0 a57.5,57.5 0 1,1 -115,0" fill="#1E1E1E" />
+        </defs>
+        <text>
+            <textPath href="#myCircle" startOffset="0%">
+                ВЫБОР &bull; ПРОФЕССИОНАЛОВ &bull;
+            </textPath>
+        </text>
+        </svg>
+    </div>
+    <div class="star">
+        <img src="images/starIcon.svg" alt="star">
+    </div>
+</div>
