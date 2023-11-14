a9f0601a5463203faccdface186ddf7c884642ee
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:38:37 +0200

    Add contact information and menu to contacts page

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,5 +1,39 @@
 <x-app-layout>
 
-    Contacts
+    <h1 class="main-title">
+        Контакты
+    </h1>
+
+    <div class="contactUs">
+        <h2>Свяжитесь с нами</h2>
+        <div class="contactUs__phone">
+
+            <x-icon-phone />
+
+            <x-phones />
+
+        </div>
+
+        <x-email-icon />
+
+        <address class="contactUs__address">
+
+            <x-icon-address />
+
+            <x-address />
+
+        </address>
+        <div class="contactUs__buttons">
+
+            <x-button-callOrder id="openOrderCall" />
+
+            <x-icon-whatsApp />
+
+            <x-icon-telegram />
+
+        </div>
+    </div>
+
+    <x-menu />
 
 </x-app-layout>
