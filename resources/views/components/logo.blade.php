901c6d61404c2e56dee2fe0e395322756c60651a
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:40:45 +0200

    Add logo component to main page

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,10 @@
+<a href="{{ route('main') }}" class="logo">
+    <x-application-logo class="logo__img" />
+    <div class="logo__text">
+        <p class="logo__text--top">Общество c ограниченной ответственностью</p>
+        <p class="logo__text--bottom">
+            <span class="logo__text--first">Ст</span>андарт
+            <span class="logo__text--first">Ст</span>роительства
+        </p>
+    </div>
+</a>
