0192be4eba0b8e247cf1c5fea5bfa90d8b78e024
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:41:38 +0200

    Add menu footer with contact icons and buttons

---
+++ resources/views/components/popup/menu-footer.blade.php
@@ -0,0 +1,17 @@
+<footer>
+
+    <x-email-icon />
+
+    <div class="right-menu">
+
+        <x-phones />
+
+        <x-button-callOrder id="openOrderCallMenu" />
+
+        <x-icon-whatsApp />
+
+        <x-icon-telegram />
+
+    </div>
+
+</footer>
