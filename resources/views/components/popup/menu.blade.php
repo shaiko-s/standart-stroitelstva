887c5c4aa1afe72902aba0a142a677b9341fc9b9
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:41:43 +0200

    Add menu popup component.

---
+++ resources/views/components/popup/menu.blade.php
@@ -0,0 +1,15 @@
+<!-- Menu PopUp -->
+<div id="popup" class="popup">
+
+    <x-popup.button-close id="closeMenu" />
+
+    <div class="popup-container">
+
+        <x-logo />
+
+        <x-menu />
+
+        <x-popup.menu-footer />
+
+    </div>
+</div>
