6c9eb40bc9e09c1123019c1f384f2b9c76b001b5
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:42:01 +0200

    Add success popup component

---
+++ resources/views/components/popup/success.blade.php
@@ -0,0 +1,15 @@
+<!-- Menu PopUp -->
+<div id="success" class="popup">
+
+    <x-popup.button-close id="closeSucess" />
+
+    <div class="popup-container">
+
+        <h3>Спасибо!</h3>
+
+        <p>Наш менеджер скоро Вам перезвонит.</p>
+
+        <img src="/images/checked_green.svg" alt="">
+
+    </div>
+</div>
