71bad9f05f9c51e57473d7c67a65ffa166cd0e18
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:35:34 +0200

    Add popups and styles for menu, order call, and send CV

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,82 @@
 import './bootstrap';
+import '../sass/fonts.sass';
+import '../sass/app.sass';
+import '../sass/nav.sass';
+import '../sass/menu.sass';
+import '../sass/main.sass';
+import '../sass/circle-spinning.sass';
+import '../sass/organization.sass';
+import '../sass/geodesy.sass';
+import '../sass/geology.sass';
+import '../sass/ecology.sass';
+import '../sass/vacancies.sass';
+import '../sass/contacts.sass';
+import '../sass/menu-popup.sass';
+import '../sass/order-call-popup.sass';
+import '../sass/send-CV-popup.sass';
+import '../sass/success.sass';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const popup = document.getElementById('popup');
+const overlay = document.getElementById('overlay');
+const openOrderCall = document.getElementById('openOrderCall');
+const openSendCV = document.getElementById('openSendCV');
+const closeSendCV = document.getElementById('closeSendCV');
+
+function toggleDisplay(ids, display) {
+    ids.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.style.display = display;
+        }
+    });
+}
+
+document.getElementById('openMenu').addEventListener('click', function() {
+    toggleDisplay(['popup', 'overlay'], 'block');
+});
+
+document.getElementById('closeMenu').addEventListener('click', function() {
+    toggleDisplay(['popup', 'overlay'], 'none');
+});
+
+document.getElementById('openOrderCallMenu').addEventListener('click', function() {
+    toggleDisplay(['orderCall', 'overlay'], 'block');
+    overlay.style.zIndex = '10';
+});
+
+document.getElementById('confirmCallOrder').addEventListener('click', function() {
+    toggleDisplay(['sucess', 'overlay'], 'block');
+});
+
+if (openOrderCall) {
+    openOrderCall.addEventListener('click', function() {
+    toggleDisplay(['orderCall', 'overlay'], 'block');
+    });
+}
+
+document.getElementById('closeOrderCall').addEventListener('click', function() {
+    if (popup && overlay && getComputedStyle(popup).display === 'block') {
+        toggleDisplay(['orderCall'], 'none');
+    } else {
+        toggleDisplay(['orderCall', 'overlay'], 'none');
+    }
+    overlay.style.zIndex = '5';
+});
+
+if (openSendCV) {
+    openSendCV.addEventListener('click', function() {
+    toggleDisplay(['sendCV', 'overlay'], 'block');
+    });
+}
+
+if (closeSendCV) {
+    closeSendCV.addEventListener('click', function() {
+        toggleDisplay(['sendCV', 'overlay'], 'none');
+    });
+}
