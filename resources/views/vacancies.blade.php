151381dd6012dc04b22520c765660d5188fba7a9
Sergey Shaiko <foodprofi@gmail.com>
2023-11-14 19:39:23 +0200

    Add "Send CV" popup and update vacancies page layout.

--- resources/views/vacancies.blade.php
+++ resources/views/vacancies.blade.php
@@ -1,5 +1,19 @@
 <x-app-layout>
 
-    Vacancies
+    <x-slot name="popup">
+        <x-popup.send-CV />
+    </x-slot>
+
+    <h1 class="main-title">
+        Вакансии
+    </h1>
+
+    <div class="surveyor vacancies-info">
+        <h2>Текущих вакансий нет</h2>
+
+        <x-button-sendCV id="openSendCV" />
+    </div>
+
+    <x-menu />
 
 </x-app-layout>
