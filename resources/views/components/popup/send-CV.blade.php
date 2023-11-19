db35ca6c0db3d71d3af4a532c9be1336f96c6497
Sergey Shaiko <foodprofi@gmail.com>
2023-11-19 22:35:03 +0100

    Refactor send-CV popup form

--- resources/views/components/popup/send-CV.blade.php
+++ resources/views/components/popup/send-CV.blade.php
@@ -9,17 +9,21 @@
 
         <p>Оставьте Ваши данные и мы свяжемся с Вами</p>
 
-        <div class="input-field">
-            <input type="text" id="name" name="name" placeholder="Введите Ваше имя" required>
-        </div>
+        <form id="sendCVForm" method="POST" action="{{ route('vacancies.store') }}" enctype="multipart/form-data">
+            @csrf
 
-        <div class="input-field">
-            <input type="tel" id="phone" name="phone" placeholder="Введите Ваш телефон" required>
-        </div>
+            <div class="input-field">
+                <input type="text" id="name" name="name" placeholder="Введите Ваше имя" required>
+            </div>
 
-        <x-button-attachCV id="attachCV" />
+            <div class="input-field">
+                <input type="tel" id="phone" name="phone" placeholder="Введите Ваш телефон" required>
+            </div>
 
-        <x-button-sendCV id="confirmSendCV" title="Отправить" />
+            <x-button-attachCV id="attachCVBtn" />
+
+            <x-button-sendCV id="confirmSendCV" title="Отправить" />
+        </form>
 
         <p class="declaration">Отправляя заявку, Вы соглашаетесь
             с <a href="#">политикой конфиденциальности</a></p>
