accb8431a1b6ec6127f52282d2c941988cfe7aae
Sergey Shaiko <foodprofi@gmail.com>
2023-11-19 22:34:33 +0100

    Refactor button-attachCV.blade.php to include dynamic id and add hidden file input

--- resources/views/components/button-attachCV.blade.php
+++ resources/views/components/button-attachCV.blade.php
@@ -1,1 +1,5 @@
-<button id="attachCV" class="attachCVBtn">Прикрепите резюме</button>
+@props(['id'])
+
+<button id={{ $id }} class="attachCVBtn" type="button">Прикрепите резюме</button>
+
+<input type="file" id="attachCVfile" name="curriculum" style="display: none;">
